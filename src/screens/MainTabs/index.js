7f30f19bf1ebbec43cdaf4e1a911ecbcd2e959c0
Clinton Owen <clint@clintonowen.com>
2019-04-30 14:52:57 -0500

    Replace tab icons w/ RN vector icons

--- src/screens/MainTabs/index.js
+++ src/screens/MainTabs/index.js
@@ -1,57 +1,71 @@
 import { Navigation } from 'react-native-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const startTabs = () => {
-  Navigation.setRoot({
-    root: {
-      bottomTabs: {
-        children: [
-          {
-            stack: {
-              children: [{
-                component: {
-                  name: 'awesome-places.FindPlaceScreen',
-                  options: {
-                    topBar: {
-                      title: {
-                        text: 'Find Place'
+  Promise.all([
+    Icon.getImageSource('md-map', 30),
+    Icon.getImageSource('ios-share-alt', 30)
+  ]).then(sources => {
+    Navigation.setRoot({
+      root: {
+        bottomTabs: {
+          children: [
+            {
+              stack: {
+                children: [{
+                  component: {
+                    name: 'awesome-places.FindPlaceScreen',
+                    options: {
+                      topBar: {
+                        title: {
+                          text: 'Find Place'
+                        }
                       }
                     }
                   }
-                }
-              }],
-              options: {
-                bottomTab: {
-                  text: 'Find Place',
-                  icon: require('../../assets/search.png')
+                }],
+                options: {
+                  bottomTab: {
+                    text: 'Find Place',
+                    textColor: 'gray',
+                    selectedTextColor: '#2196F3',
+                    icon: sources[0],
+                    iconColor: 'gray',
+                    selectedIconColor: '#2196F3'
+                  }
                 }
               }
-            }
-          },
-          {
-            stack: {
-              children: [{
-                component: {
-                  name: 'awesome-places.SharePlaceScreen',
-                  options: {
-                    topBar: {
-                      title: {
-                        text: 'Share Place'
+            },
+            {
+              stack: {
+                children: [{
+                  component: {
+                    name: 'awesome-places.SharePlaceScreen',
+                    options: {
+                      topBar: {
+                        title: {
+                          text: 'Share Place'
+                        }
                       }
                     }
                   }
-                }
-              }],
-              options: {
-                bottomTab: {
-                  text: 'Share Place',
-                  icon: require('../../assets/search.png')
+                }],
+                options: {
+                  bottomTab: {
+                    text: 'Share Place',
+                    textColor: 'gray',
+                    selectedTextColor: '#2196F3',
+                    icon: sources[1],
+                    iconColor: 'gray',
+                    selectedIconColor: '#2196F3'
+                  }
                 }
               }
             }
-          }
-        ]
+          ]
+        }
       }
-    }
+    });
   });
 };
 
